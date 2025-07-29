553742ffe0090d1a45a6758e8f9d5af5e1c74a41
hsw1920 <hsw1920@gmail.com>
2025-07-29 21:53:22 +0900

    [PC-1085] Build: 1.0.1 테스트 플라이트 배포

--- Tuist/ProjectDescriptionHelpers/AppContants.swift
+++ Tuist/ProjectDescriptionHelpers/AppContants.swift
@@ -12,8 +12,8 @@ public enum AppConstants {
   public static let organizationName: String = "puzzly"
   public static let bundleId: String = "com.puzzly.piece"
   public static let bundleDisplayName: Plist.Value = "피스"
-  public static let version: Plist.Value = "1.0.0"
-  public static let build: Plist.Value = "6"
+  public static let version: Plist.Value = "1.0.1"
+  public static let build: Plist.Value = "1"
   public static let destinations: Set<Destination> = [.iPhone]
   public static let deploymentTargets: DeploymentTargets = .iOS("17.0")
 }
