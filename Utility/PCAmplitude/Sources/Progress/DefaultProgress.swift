9e5c59dbc42b84f837902a60f94bc111c0ed8e77
hsw1920 <hsw1920@gmail.com>
2025-09-15 21:24:12 +0900

    [PC-753] Feature: Login 관련 Amplitude 추가
    - 로그인 관련 단일 화면 DefaultProgress에 추가 및 적용
    - Alert는 예외적으로 싱글톤 PCAmplitude를 직접 호출(onChange에서 트리거)

--- Utility/PCAmplitude/Sources/Progress/DefaultProgress.swift
+++ Utility/PCAmplitude/Sources/Progress/DefaultProgress.swift
@@ -9,4 +9,7 @@ import Foundation
 
 public enum DefaultProgress: String, ProgressTrackable {
   case termsDetail = "signup_terms_detail"
+  case loginIntro = "login_intro"
+  case loginPhoneVerify = "login_phone_verify"
+  case alreadyRegisteredPopup = "already_registered_popup"
 }
