aecd1e7c0df4421833cdb921e04791f8b9bed135
hsw1920 <hsw1920@gmail.com>
2025-10-25 19:23:28 +0900

    [PC-1311] Feature: 프로필 심사 거절 구현
    - 다중 매칭 UI에서 리뉴얼되어 사용될 프로필 심사 거절 비즈니스 로직과 UI 구현
    - ProfileRejectedViewModel은 ProfileRejectedViewState와 ProfileRejectedReason enum을 통해 상태 관리

--- Presentation/Feature/MatchingMain/Sources/MatchingMain/View/ProfileRejectedViewModel.swift
+++ Presentation/Feature/MatchingMain/Sources/MatchingMain/View/ProfileRejectedViewModel.swift
@@ -12,11 +12,26 @@ import UseCases
 @MainActor
 @Observable
 final class ProfileRejectedViewModel {
+  enum ProfileRejectedViewState: Equatable {
+    case loading
+    case success
+  }
+  
+  enum ProfileRejectedReason: Equatable {
+    case all
+    case image
+    case valueTalk
+  }
+  
   enum Action {
     case onAppear
   }
   
   private let getUserRejectUseCase: GetUserRejectUseCase
+  
+  var viewState: ProfileRejectedViewState = .loading
+  var rejectedReason: ProfileRejectedReason = .all
+  
   init(getUserRejectUseCase: GetUserRejectUseCase) {
     self.getUserRejectUseCase = getUserRejectUseCase
   }
@@ -29,9 +44,33 @@ final class ProfileRejectedViewModel {
   }
   
   private func handleOnAppear() {
+    Task {
+      viewState = .loading
+      rejectedReason = await fetchUserRejectState()
+      viewState = .success
+    }
   }
   
   private func fetchUserRejectState() async -> ProfileRejectedReason {
+    do {
+      let userRejectState = try await getUserRejectUseCase.execute()
+      switch (userRejectState.reasonImage, userRejectState.reasonValues) {
+      case (true, true):
+        return .all
+        
+      case (true, false):
+        return .image
+        
+      case (false, true):
+        return .valueTalk
+        
+      case (false, false):
+        return .all
+      }
+    } catch {
+      print("ERROR: \(error.localizedDescription)")
+      return .all
+    }
   }
 
 }
